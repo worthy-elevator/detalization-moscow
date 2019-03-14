dc6a5db71dcf203d3bdcd15f243831351c8b4331
worthy-elevator <emil_96@inbox.ru>
2019-03-14 17:05:24 +0300

    header first

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+
+
+?>
+
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+  <script type="text/javascript" src="https://vk.com/js/api/openapi.js?160"></script>
+  <link href="css/style.css" type="text/css" rel="stylesheet">
+  <meta name="description" content="" />
+  <meta name="keywords" content="деталь штамповка, деталь штамповка симферополь, деталь заказчик чертеж крым, металлообработка заказ крым, деталь изготовление чертеж, деталь изготовление	крым, деталь заказчик изготовление чертеж, фрезерные работы, услуга металлообработка, металл штамповка, заказ работа токарный крым, работа токарный фрезерный, деталь заказчик чертеж, деталь заказчик изготовление чертеж, металлообработка чертеж	симферополь, листовой металл штамповка симферополь, деталь изготовление чертеж, механический обработка крым, работа токарный фрезерный, деталь изготовление, заказ деталь, услуга металлообработка, токарный заказ, токарная обработка крым, токарная обработка, обработка на токарных станках, токарно фрезерная обработка, токарная обработка металла, токарная обработка деталей, токарная обработка цилиндрических поверхностей, токарная обработка резанием, токарная обработка наружных поверхностей, токарная обработка стали, токарная обработка чпу, токарные работы обработка, обработка поверхностей +на токарных станках, токарная обработка вала, токарно фрезерная обработка деталей, " /> 
+  <title>Компас-Деталь</title>
+  <link rel="shortcut icon" href="/img/1.ico" type="image/x-icon">
+</head>
+<body>
+<div id="body">
+
+	<div class="header" style="background-color: antiquewhite; width: 100vw;">
+
+		<div class="top-line" style="width: 100vw; height: 45px; background-color: blue; position: fixed; top: 0;">
+			<div class="bar-nav-buttons" style="width: 900px; margin: 0 auto;">
+				<div class="nav-mail" style="float: left; padding:10 5;">
+			        <a href="tel:+7(978)XXXXXXX">тел. +7(978)XXXXXXX</a>
+				</div>
+				<div class="nav-phone" style="float: left; padding:10 5;">
+					<a href="mailto:yourmail@gmail ">email: yourmail@gmail </a>
+				</div>
+				<div class="nav-button" style="float: right; padding:10 5;">
+					<a href="#">О компании</a>
+				</div>
+				<div class="nav-button" style="float: right; padding:10 5;">
+					<a href="#">Выполненые работы</a>
+				</div>
+				<div class="nav-button" style="float: right; padding:10 5;">
+					<a href="#">Контакты</a>	
+				</div>
+			</div>
+		</div>
+		<div class="nav-bar" style="width: 900px; height: 100px;  padding:5 10; margin: 45px auto 0 auto;">
+			<div class="header-label" style="width: 65px; display: inline-block;">
+				<img src="img/logo.png" style="width: 65px; display: inline-block;">
+			</div>
+			<div class="header-worktime" style="display: inline-block; width: 210px; margin: 0 10;">
+				<p>Режим работы</p>
+				<img src="img/logo.png" style="width: 20px;display: inline-block;">
+				<div class="set-worktime" style="display: inline-block;">
+					<p >c восхода и до рассвета</p>
+					<p >c восхода и до рассвета</p>
+				</div>
+			</div>
+			<div class="header-callback" style="display: inline-block; width: 200px; ">
+				<div class="header-callback-button" style="width: 150px; height: 20px; margin:auto; text-align: center;">
+					<p>Заказать звонок</p>
+				</div>
+			</div>
+			<div class="header-phone-number" style="display: inline-block;">
+				<p>Отдел продаж(Москва):</p>
+			    <a href="tel:+7(978)XXXXXXX">тел. +7(978)XXXXXXX</a>
+			</div>
+			<div class="header-phone-number" style="display: inline-block;">
+				<p>По росии:</p>
+			    <a href="tel:+7(978)XXXXXXX">тел. +7(978)XXXXXXX</a>
+			</div>
+			<div class="header-menu" style="position: relative; bottom: -15px; width: 617px; padding: 10; border: 1px solid black; border-radius: 5px; margin:10 auto; background-color: aqua;">
+				<div class="header-menu-button" style="display: inline-block; border-right: 1px solid black; padding:0 15;">
+					<a href="">обработка</a>
+				</div>
+				<div class="header-menu-button" style="display: inline-block; border-right: 1px solid black; padding:0 15;">
+					<a href="">доработка</a>
+				</div>
+				<div class="header-menu-button" style="display: inline-block; border-right: 1px solid black; padding:0 15;">
+					<a href="">переработка</a>
+				</div>
+				<div class="header-menu-button" style="display: inline-block; border-right: 1px solid black; padding:0 15;">
+					<a href="">выработка</a>
+				</div>
+				<div class="header-menu-button" style="display: inline-block; padding:0 15;">
+					<a href="">издевательство</a>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+</div>
+</body>
